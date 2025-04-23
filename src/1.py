b260b740d7f671535a2f9e8685aeea27c8e5290d
Bin.Alex <86982162+BinAlexme@users.noreply.github.com>
2025-04-23 18:48:04 +0300

    Update 1.py

--- src/1.py
+++ src/1.py
@@ -1,11 +1,12 @@
-def average_num(list_num: list) -> float:
+def average_num(list_num: list) -> str | float:
     for ind, el in enumerate(list_num):
-        if not isinstance(el, int | float):
+        if not isinstance(el, (int, float)):
             try:
                 list_num[ind] = int(el)
-            except (ValueError, TypeError):
+            except TypeError:
                 return "Bad request"
     return round(sum(list_num) / len(list_num), 2)
+
 
 
 # assert
